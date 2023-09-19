51683e9cfd6f8d246599d4603da55ebbfec22369
tnaul <pvluan225@gmail.com>
2023-09-19 20:31:12 +0700

    fix UI file

--- gradio_ui.py
+++ gradio_ui.py
@@ -1,33 +1,5 @@
 import gradio as gr
-# import os
-# os.system('python -m spacy download en_core_web_sm')
-import spacy
-from spacy import displacy
-
 from index import setup_ner
-
-# nlp = spacy.load("en_core_web_sm")
-
-# def text_analysis(text):
-#     doc = nlp(text)
-#     html = displacy.render(doc, style="dep", page=True)
-#     html = (
-#         "<div style='max-width:100%; max-height:360px; overflow:auto'>"
-#         + html
-#         + "</div>"
-#     )
-#     pos_count = {
-#         "char_count": len(text),
-#         "token_count": 0,
-#     }
-#     pos_tokens = []
-
-#     for token in doc:
-#         print(token.text, token.pos_)
-#         pos_tokens.extend([(token.text, token.pos_), (" ", None)])
-#     print(pos_tokens)
-
-#     return pos_tokens, pos_count, html
 
 classifier = setup_ner(cache_dir='./cache_models/')
 
